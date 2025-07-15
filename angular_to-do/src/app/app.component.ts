248f7c404bb911cf332f8af206b6b97a1c7aedad
Officialakshat <165244500+Officialakshat@users.noreply.github.com>
2025-07-15 21:25:32 +0530

    update

--- angular_to-do/src/app/app.component.ts
+++ angular_to-do/src/app/app.component.ts
@@ -3,11 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { TodoComponent } from './components/todo/todo.component';
 // import { BrowserModule } from '@angular/platform-browser';
 import { HeaderComponent } from './header/header.component';
+import { ReminderComponent } from './components/reminder/reminder.component';
+import { ArchieveComponent } from './components/archieve/archieve.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, TodoComponent],
+  imports: [RouterOutlet, HeaderComponent, TodoComponent, ReminderComponent, ArchieveComponent, ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
---
+++ angular_to-do/src/app/components/archieve/archieve.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-archieve',
+  imports: [],
+  templateUrl: './archieve.component.html',
+  styleUrl: './archieve.component.css'
+})
+export class ArchieveComponent {
+
+}
