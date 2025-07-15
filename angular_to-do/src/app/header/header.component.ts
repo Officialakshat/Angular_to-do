813d41fa74c8a95447fda00dd506cadd8c2baf61
Officialakshat <165244500+Officialakshat@users.noreply.github.com>
2025-07-15 17:02:28 +0530

    ss

--- angular_to-do/src/app/header/header.component.ts
+++ angular_to-do/src/app/header/header.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
-// import { RouterOutlet } from "../../../node_modules/@angular/router/router_module.d-Bx9ArA6K";
-import { TodoComponent } from '../components/todo/todo.component';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-header',
+  standalone: true,
   imports: [],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-
+  toggleDarkMode() {
+    document.body.classList.toggle('dark');
+  }
 }
