f614aa679c9781b832eaa6162f0c426a21010460
Officialakshat <165244500+Officialakshat@users.noreply.github.com>
2025-07-19 21:50:08 +0530

    update

--- angular_to-do/src/app/header/header.component.ts
+++ angular_to-do/src/app/header/header.component.ts
@@ -1,7 +1,10 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
+  imports: [NgIf, RouterLink],
   standalone: true,
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
